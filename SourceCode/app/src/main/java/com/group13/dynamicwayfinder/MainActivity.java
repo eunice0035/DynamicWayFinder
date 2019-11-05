d8578e813b12950a31b97d5cea7068be2b58bd7a
Bryan Tyrrell <b.tyrrell2@nuigalway.ie>
2019-11-05 11:43:56 +0000

    05/11/2019 11:43 updated comments in file to check slack is working

--- SourceCode/app/src/main/java/com/group13/dynamicwayfinder/MainActivity.java
+++ SourceCode/app/src/main/java/com/group13/dynamicwayfinder/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-// Starting class
+// Starting class activity
 public class MainActivity extends AppCompatActivity {
 
     @Override
